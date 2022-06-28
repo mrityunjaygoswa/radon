29ef5f707c070afb6a18b259d139ea4ed597716e
MrMohit08 <44553552+MrMohit08@users.noreply.github.com>
2022-06-28 18:33:00 +0530

    initial update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://mohit08:BMqbHFaOsEjDesmP@cluster0.btwze.mongodb.net/Project-02", {
+    useNewUrlParser: true
+})
+.then( () => console.log("WELCOME Mr. Mohit , MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});

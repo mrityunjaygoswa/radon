5c343e1b2f8aca45080704ea45a174277e498a0f
mrityunjaygoswa <mrityunjay.giri007@gmail.com>
2022-11-22 09:48:54 +0530

    dine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+
+   const [input,inputList] = useState()
+   const [item,setItem] = useState([])
+  const itemEvent=(Event)=>{
+   inputList(Event.target.value)
+  }
+  const listOfItems =()=>{
+   setItem((arr)=>{
+      return [...item,input]
+   })
+   inputList("")
+  }
+
+return (
+
+ <div className="centre-Div">
+    <br />
+    <h1 className="h1" >To Do List</h1>  
+   <br />
+   <input type="text" placeholder="add a task" className="input" onChange={itemEvent} value={input}></input>    
+   <button  className="button" onClick={listOfItems}> + </button>
+   <br/>
+   <ol>
+   {
+      item.map((val)=>{
+         return (<>
+            <div className="toDo_Style">
+         <i class="fa-Times" are-hidded="true"/>
+            <li>{val}</li>
+            </div>
+            </>)
+      })
+   }
+   
+   </ol>
+ </div>
+
+    )
+
 }
-
-export default App;
+export default App
